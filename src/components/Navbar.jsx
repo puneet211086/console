6778616d8dd7b90d4733fd34aa61b3b2a703f4ba
Puneet <puneet211086@gmail.com>
2024-01-12 18:09:58 -0500

    added hamburger menu for smaller screens

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,35 +1,62 @@
-import React from 'react';
-import { Box,AppBar, Toolbar, Button, Typography } from '@mui/material';
+import React, {useState} from 'react';
+import { Box,AppBar, Toolbar, Button, Typography, IconButton, Drawer, List, ListItem, useTheme, useMediaQuery,ListItemIcon,ListItemText } from '@mui/material';
 import ComputerIcon from '@mui/icons-material/Computer';
 import ContactPhoneIcon from '@mui/icons-material/ContactPhone';
 import StorageIcon from '@mui/icons-material/Storage';
 import CloudIcon from '@mui/icons-material/Cloud';
 import ToolsIcon from '@mui/icons-material/Build'; 
+import MenuIcon from '@mui/icons-material/Menu';
 
 
 const Navbar = ({ setActiveComponent }) => {
+  const [drawerOpen, setDrawerOpen] = useState(false);
+  const theme = useTheme();
+  const isMobile = useMediaQuery(theme.breakpoints.down('md'));
 
-  const handleVMwareClick = () => {
-    setActiveComponent('nestedList');
+  const toggleDrawer = (open) => (event) => {
+    if (event.type === 'keydown' && (event.key === 'Tab' || event.key === 'Shift')) {
+      return;
+    }
+    setDrawerOpen(open);
   };
 
-  const handleVendorClick = () => {
-    setActiveComponent('vendorPage');
-  };
-  const handleSSMToolsClick = () => {
-    setActiveComponent('ssmTools');
+  const handleButtonClick = (component) => {
+    setActiveComponent(component);
+    if (isMobile) setDrawerOpen(false);
   };
 
-  const handleStorageInfoClick = () => {
-    setActiveComponent('storageInfo')
-  };
+  const drawerList = (
+    <List>
+      <ListItem button onClick={() => handleButtonClick('nestedList')}>
+      <ListItemIcon><ComputerIcon /></ListItemIcon>
+      <ListItemText primary="VMware" sx={{ marginLeft: 1 }} />
+      </ListItem>
 
-  const handleAzureInfoClick = () => {
-    setActiveComponent('azureInfo')
-  };
+      <ListItem button onClick={() => handleButtonClick('vendorPage')}>
+      <ListItemIcon><ContactPhoneIcon /></ListItemIcon>
+      <ListItemText primary="Vendor Contact" sx={{ marginLeft: 1 }} />
+        
+      </ListItem>
+      <ListItem button onClick={() => handleButtonClick('ssmTools')}>
+      <ListItemIcon><ToolsIcon /></ListItemIcon>
+      <ListItemText primary="SSM Tools" sx={{ marginLeft: 1 }} />
+      
+      </ListItem>
+      <ListItem button onClick={() => handleButtonClick('storageInfo')}>
+      <ListItemIcon><StorageIcon /></ListItemIcon>
+      <ListItemText primary="Storage Info" sx={{ marginLeft: 1 }} />
+        
+      </ListItem>
+      <ListItem button onClick={() => handleButtonClick('azureInfo')}>
+      <ListItemIcon><CloudIcon /></ListItemIcon>
+      <ListItemText primary="Azure Info" sx={{ marginLeft: 1 }} />
+      </ListItem>
+    </List>
+  );
 
 
-  return (
+return (
+  <>
     <AppBar position="static">
       <Toolbar>
       <Box sx={{ flexGrow: 1, display: 'flex', alignItems: 'center' }}>
@@ -38,15 +65,30 @@ const Navbar = ({ setActiveComponent }) => {
             CloudBuildRepeat
           </Typography>
         </Box>
-        <Button color="inherit" startIcon={<ComputerIcon fontSize="small"/>} onClick={handleVMwareClick}>VMware</Button>
-        <Button color="inherit" startIcon={<ContactPhoneIcon fontSize="large"/>} onClick={handleVendorClick}>Vendor Contact</Button>
-        
-        <Button color="inherit" startIcon={<ToolsIcon fontSize="large"/>} onClick={handleSSMToolsClick}>SSM Tools</Button>
-        <Button color="inherit" startIcon={<StorageIcon fontSize="large"/>} onClick={handleStorageInfoClick}>Storage Info</Button>
-        <Button color="inherit" startIcon={<CloudIcon fontSize="large"/>} onClick={handleAzureInfoClick}>Azure Info</Button>
+        {isMobile && (
+          <IconButton edge="start" color="inherit" aria-label="menu" onClick={toggleDrawer(true)} sx={{ mr: 2 }}>
+            <MenuIcon />
+          </IconButton>
+        )}
+       
+        {!isMobile && (
+          <>
+            <Button color="inherit" startIcon={<ComputerIcon fontSize="small"/>} onClick={() => handleButtonClick('nestedList')} >VMware</Button>
+            <Button color="inherit" startIcon={<ContactPhoneIcon fontSize="large"/>} onClick={() => handleButtonClick('vendorPage')}>Vendor Contact</Button>
+            <Button color="inherit" startIcon={<ToolsIcon fontSize="large"/>} onClick={() => handleButtonClick('ssmTools')}>SSM Tools</Button>
+            <Button color="inherit" startIcon={<StorageIcon fontSize="large"/>} onClick={() => handleButtonClick('storageInfo')}>Storage Info</Button>
+            <Button color="inherit" startIcon={<CloudIcon fontSize="large"/>} onClick={() => handleButtonClick('azureInfo')}>Azure Info</Button>
+          </>
+        )}
       </Toolbar>
     </AppBar>
-  );
+    <Drawer anchor="left" open={drawerOpen} onClose={toggleDrawer(false)}>
+      {drawerList}
+    </Drawer>
+  </>
+);
 };
 
 export default Navbar;
+
+
