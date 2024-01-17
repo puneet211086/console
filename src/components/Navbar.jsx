3c820e5322e3c9651eef8017d580bba096bb6a77
Puneet <puneet211086@gmail.com>
2024-01-17 07:24:59 -0500

    replaced navbar butttons with Tabs

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,21 +1,38 @@
-import React, {useState} from 'react';
-import { Box,AppBar, Toolbar, Button, Typography, IconButton, Drawer, List, ListItem, useTheme, useMediaQuery,ListItemIcon,ListItemText } from '@mui/material';
-import ComputerIcon from '@mui/icons-material/Computer';
-import ContactPhoneIcon from '@mui/icons-material/ContactPhone';
-import StorageIcon from '@mui/icons-material/Storage';
-import CloudIcon from '@mui/icons-material/Cloud';
-import ToolsIcon from '@mui/icons-material/Build'; 
-import MenuIcon from '@mui/icons-material/Menu';
-import LocalFireDepartmentTwoToneIcon from '@mui/icons-material/LocalFireDepartmentTwoTone';
-
+import React, { useState } from "react";
+import {
+  Box,
+  AppBar,
+  Toolbar,
+  Typography,
+  IconButton,
+  Drawer,
+  List,
+  ListItem,
+  useTheme,
+  useMediaQuery,
+  ListItemIcon,
+  ListItemText,
+  Tab,
+  Tabs,
+} from "@mui/material";
+import ComputerIcon from "@mui/icons-material/Computer";
+import ContactPhoneIcon from "@mui/icons-material/ContactPhone";
+import StorageIcon from "@mui/icons-material/Storage";
+import CloudIcon from "@mui/icons-material/Cloud";
+import ToolsIcon from "@mui/icons-material/Build";
+import MenuIcon from "@mui/icons-material/Menu";
+import LocalFireDepartmentTwoToneIcon from "@mui/icons-material/LocalFireDepartmentTwoTone";
 
 const Navbar = ({ setActiveComponent }) => {
   const [drawerOpen, setDrawerOpen] = useState(false);
   const theme = useTheme();
-  const isMobile = useMediaQuery(theme.breakpoints.down('lg'));
-
+  const isMobile = useMediaQuery(theme.breakpoints.down("lg"));
+  const [tabValue, setTabValue] = useState(null);
   const toggleDrawer = (open) => (event) => {
-    if (event.type === 'keydown' && (event.key === 'Tab' || event.key === 'Shift')) {
+    if (
+      event.type === "keydown" &&
+      (event.key === "Tab" || event.key === "Shift")
+    ) {
       return;
     }
     setDrawerOpen(open);
@@ -28,71 +45,110 @@ const Navbar = ({ setActiveComponent }) => {
 
   const drawerList = (
     <List>
-      <ListItem button onClick={() => handleButtonClick('nestedList')}>
-      <ListItemIcon><ComputerIcon /></ListItemIcon>
-      <ListItemText primary="VMware" sx={{ marginLeft: 1 }} />
+      <ListItem button onClick={() => handleButtonClick("nestedList")}>
+        <ListItemIcon>
+          <ComputerIcon />
+        </ListItemIcon>
+        <ListItemText primary="VMware" sx={{ marginLeft: 1 }} />
       </ListItem>
 
-      <ListItem button onClick={() => handleButtonClick('vendorPage')}>
-      <ListItemIcon><ContactPhoneIcon /></ListItemIcon>
-      <ListItemText primary="Vendor Contact" sx={{ marginLeft: 1 }} />
-        
+      <ListItem button onClick={() => handleButtonClick("vendorPage")}>
+        <ListItemIcon>
+          <ContactPhoneIcon />
+        </ListItemIcon>
+        <ListItemText primary="Vendor Contact" sx={{ marginLeft: 1 }} />
       </ListItem>
-      <ListItem button onClick={() => handleButtonClick('ssmTools')}>
-      <ListItemIcon><ToolsIcon /></ListItemIcon>
-      <ListItemText primary="SSM Tools" sx={{ marginLeft: 1 }} />
-      
+      <ListItem button onClick={() => handleButtonClick("ssmTools")}>
+        <ListItemIcon>
+          <ToolsIcon />
+        </ListItemIcon>
+        <ListItemText primary="SSM Tools" sx={{ marginLeft: 1 }} />
       </ListItem>
-      <ListItem button onClick={() => handleButtonClick('storageInfo')}>
-      <ListItemIcon><StorageIcon /></ListItemIcon>
-      <ListItemText primary="Storage Info" sx={{ marginLeft: 1 }} />
-        
+      <ListItem button onClick={() => handleButtonClick("storageInfo")}>
+        <ListItemIcon>
+          <StorageIcon />
+        </ListItemIcon>
+        <ListItemText primary="Storage Info" sx={{ marginLeft: 1 }} />
       </ListItem>
-      <ListItem button onClick={() => handleButtonClick('azureInfo')}>
-      <ListItemIcon><CloudIcon /></ListItemIcon>
-      <ListItemText primary="Azure Info" sx={{ marginLeft: 1 }} />
+      <ListItem button onClick={() => handleButtonClick("azureInfo")}>
+        <ListItemIcon>
+          <CloudIcon />
+        </ListItemIcon>
+        <ListItemText primary="Azure Info" sx={{ marginLeft: 1 }} />
       </ListItem>
     </List>
   );
 
+  return (
+    <>
+      <AppBar position="static" sx={{ bgcolor: "#063970" }}>
+        <Toolbar>
+          <Box sx={{ flexGrow: 1, display: "flex", alignItems: "center" }}>
+            {/* <img src="https://www.freepnglogos.com/uploads/company-logo-png/company-logo-telestream-press-kit-12.png" alt="Company Logo" style={{ height: '50px', marginRight: '10px' }} /> */}
+            <LocalFireDepartmentTwoToneIcon fontSize="large" />
+            <Typography variant="h6"></Typography>
+          </Box>
+          {isMobile && (
+            <IconButton
+              edge="start"
+              color="inherit"
+              aria-label="menu"
+              onClick={toggleDrawer(true)}
+              sx={{ mr: 2 }}
+            >
+              <MenuIcon />
+            </IconButton>
+          )}
 
-return (
-  <>
-    <AppBar position="static">
-      <Toolbar>
-      <Box sx={{ flexGrow: 1, display: 'flex', alignItems: 'center' }}>
-          {/* <img src="https://www.freepnglogos.com/uploads/company-logo-png/company-logo-telestream-press-kit-12.png" alt="Company Logo" style={{ height: '50px', marginRight: '10px' }} /> */}
-          <LocalFireDepartmentTwoToneIcon fontSize='large' />
-          <Typography variant="h6">
-
-          </Typography>
-        </Box>
-        {isMobile && (
-          <IconButton edge="start" color="inherit" aria-label="menu" onClick={toggleDrawer(true)} sx={{ mr: 2 }}>
-            <MenuIcon />
-          </IconButton>
-        )}
-       
-        {!isMobile && (
-          <>
-            <Button sx={{mr:2}} variant='contained' color='info' startIcon={<ComputerIcon fontSize="small"/>} onClick={() => handleButtonClick('nestedList')} >VMware</Button>
-            <Button sx={{mr:2}} variant='contained' color='info' startIcon={<ContactPhoneIcon fontSize="large"/>} onClick={() => handleButtonClick('vendorPage')}>Vendor Contact</Button>
-            <Button sx={{mr:2}} variant='contained' color='info' startIcon={<ToolsIcon fontSize="large"/>} onClick={() => handleButtonClick('ssmTools')}>SSM Tools</Button>
-            <Button sx={{mr:2}} variant='contained' color='info' startIcon={<StorageIcon fontSize="large"/>} onClick={() => handleButtonClick('storageInfo')}>Storage Info</Button>
-            <Button sx={{mr:2}} variant='contained' color='info' startIcon={<CloudIcon fontSize="large"/>} onClick={() => handleButtonClick('azureInfo')}>Azure Info</Button>
-          </>
-        )}
-      </Toolbar>
-    </AppBar>
-    <Drawer anchor="left"  colo open={drawerOpen} onClose={toggleDrawer(false) } sx={{
-          '& .MuiDrawer-paper': { backgroundColor: 'lightgray' } // Set drawer background color here
-        }}>
-      {drawerList}
-    </Drawer>
-  </>
-);
+          {!isMobile && (
+            <Tabs
+              value={tabValue}
+              onChange={(e, tabValue) => setTabValue(tabValue)}
+              textColor="inherit"
+              indicatorColor="secondary"
+            >
+              <Tab
+                onClick={() => handleButtonClick("nestedList")}
+                label="vmware"
+                icon={<ComputerIcon fontSize="small" />}
+              />
+              <Tab
+                onClick={() => handleButtonClick("vendorPage")}
+                label="Vendor Contact"
+                icon={<ContactPhoneIcon fontSize="small" />}
+              />
+              <Tab
+                onClick={() => handleButtonClick("ssmTools")}
+                label="SSM Tools"
+                icon={<ToolsIcon fontSize="small" />}
+              />
+              <Tab
+                onClick={() => handleButtonClick("storageInfo")}
+                label="Storage Info"
+                icon={<StorageIcon fontSize="small" />}
+              />
+              <Tab
+                onClick={() => handleButtonClick("azureInfo")}
+                label="Azure Info"
+                icon={<CloudIcon fontSize="small" />}
+              />
+            </Tabs>
+          )}
+        </Toolbar>
+      </AppBar>
+      <Drawer
+        anchor="left"
+        colo
+        open={drawerOpen}
+        onClose={toggleDrawer(false)}
+        sx={{
+          "& .MuiDrawer-paper": { backgroundColor: "lightgray" }, // Set drawer background color here
+        }}
+      >
+        {drawerList}
+      </Drawer>
+    </>
+  );
 };
 
 export default Navbar;
-
-
--- src/components/NestedList.jsx
+++ src/components/NestedList.jsx
@@ -1,116 +1,243 @@
-import React, { useState } from 'react';
-import { List, ListItem, ListItemText, Collapse, Link ,Typography} from '@mui/material';
-import ExpandLess from '@mui/icons-material/ExpandLess';
-import ExpandMore from '@mui/icons-material/ExpandMore';
-import DataCenterIcon from '@mui/icons-material/ApartmentTwoTone';
-import VcenterIcon from '@mui/icons-material/AccountTreeTwoTone';
-import IloIcon from '@mui/icons-material/SettingsRemote';
-import HostIcon from '@mui/icons-material/SpeakerTwoTone';
-import ClusterIcon from '@mui/icons-material/SpeakerGroupTwoTone';
+import React, { useState } from "react";
+import {
+  List,
+  ListItem,
+  ListItemText,
+  Collapse,
+  Link,
+  Typography,
+} from "@mui/material";
+import ExpandLess from "@mui/icons-material/ExpandLess";
+import ExpandMore from "@mui/icons-material/ExpandMore";
+import DataCenterIcon from "@mui/icons-material/ApartmentTwoTone";
+import VcenterIcon from "@mui/icons-material/AccountTreeTwoTone";
+import IloIcon from "@mui/icons-material/SettingsRemote";
+import HostIcon from "@mui/icons-material/SpeakerTwoTone";
+import ClusterIcon from "@mui/icons-material/SpeakerGroupTwoTone";
 
-import jsonData from '../dataFiles/vCenterData.json'; 
-
+import jsonData from "../dataFiles/vCenterData.json";
 
 const NestedList = () => {
-  const [openStates, setOpenStates] = useState(jsonData.map(vcenter => ({
-    vcenter: false,
-    datacenters: vcenter.datacenters.map(datacenter => ({
-      datacenter: false,
-      clusters: datacenter.clusters.map(cluster => ({
-        cluster: false,
-        hosts: cluster.hosts.map(() => false)
-      }))
+  const [openStates, setOpenStates] = useState(
+    jsonData.map((vcenter) => ({
+      vcenter: false,
+      datacenters: vcenter.datacenters.map((datacenter) => ({
+        datacenter: false,
+        clusters: datacenter.clusters.map((cluster) => ({
+          cluster: false,
+          hosts: cluster.hosts.map(() => false),
+        })),
+      })),
     }))
-  })));
+  );
 
   const toggleVcenter = (vIdx) => {
-    setOpenStates(openStates.map((vcenter, idx) => idx === vIdx ? 
-      { ...vcenter, vcenter: !vcenter.vcenter } : vcenter));
+    setOpenStates(
+      openStates.map((vcenter, idx) =>
+        idx === vIdx ? { ...vcenter, vcenter: !vcenter.vcenter } : vcenter
+      )
+    );
   };
 
   const toggleDatacenter = (vIdx, dIdx) => {
-    setOpenStates(openStates.map((vcenter, vi) => vi === vIdx ? 
-      { 
-        ...vcenter, 
-        datacenters: vcenter.datacenters.map((datacenter, di) => di === dIdx ? 
-          { ...datacenter, datacenter: !datacenter.datacenter } : datacenter)
-      } : vcenter));
+    setOpenStates(
+      openStates.map((vcenter, vi) =>
+        vi === vIdx
+          ? {
+              ...vcenter,
+              datacenters: vcenter.datacenters.map((datacenter, di) =>
+                di === dIdx
+                  ? { ...datacenter, datacenter: !datacenter.datacenter }
+                  : datacenter
+              ),
+            }
+          : vcenter
+      )
+    );
   };
 
   const toggleCluster = (vIdx, dIdx, cIdx) => {
-    setOpenStates(openStates.map((vcenter, vi) => vi === vIdx ? 
-      { 
-        ...vcenter, 
-        datacenters: vcenter.datacenters.map((datacenter, di) => di === dIdx ? 
-          {
-            ...datacenter, 
-            clusters: datacenter.clusters.map((cluster, ci) => ci === cIdx ? 
-              { ...cluster, cluster: !cluster.cluster } : cluster)
-          } : datacenter)
-      } : vcenter));
+    setOpenStates(
+      openStates.map((vcenter, vi) =>
+        vi === vIdx
+          ? {
+              ...vcenter,
+              datacenters: vcenter.datacenters.map((datacenter, di) =>
+                di === dIdx
+                  ? {
+                      ...datacenter,
+                      clusters: datacenter.clusters.map((cluster, ci) =>
+                        ci === cIdx
+                          ? { ...cluster, cluster: !cluster.cluster }
+                          : cluster
+                      ),
+                    }
+                  : datacenter
+              ),
+            }
+          : vcenter
+      )
+    );
   };
 
   const toggleHost = (vIdx, dIdx, cIdx, hIdx) => {
-    setOpenStates(openStates.map((vcenter, vi) => vi === vIdx ? 
-      { 
-        ...vcenter, 
-        datacenters: vcenter.datacenters.map((datacenter, di) => di === dIdx ? 
-          {
-            ...datacenter, 
-            clusters: datacenter.clusters.map((cluster, ci) => ci === cIdx ? 
-              { 
-                ...cluster, 
-                hosts: cluster.hosts.map((host, hi) => hi === hIdx ? !host : host) 
-              } : cluster)
-          } : datacenter)
-      } : vcenter));
+    setOpenStates(
+      openStates.map((vcenter, vi) =>
+        vi === vIdx
+          ? {
+              ...vcenter,
+              datacenters: vcenter.datacenters.map((datacenter, di) =>
+                di === dIdx
+                  ? {
+                      ...datacenter,
+                      clusters: datacenter.clusters.map((cluster, ci) =>
+                        ci === cIdx
+                          ? {
+                              ...cluster,
+                              hosts: cluster.hosts.map((host, hi) =>
+                                hi === hIdx ? !host : host
+                              ),
+                            }
+                          : cluster
+                      ),
+                    }
+                  : datacenter
+              ),
+            }
+          : vcenter
+      )
+    );
   };
 
   return (
     <List>
-    <Typography variant="h5" sx={{ textAlign: 'left', mt: 2 ,mb:4}}>VMware vCenter Server List</Typography>
+      <Typography variant="h5" sx={{ textAlign: "left", mt: 2, mb: 4 }}>
+        VMware vCenter Server List
+      </Typography>
       {jsonData.map((vcenter, vIdx) => (
         <React.Fragment key={vIdx}>
-          <ListItem button onClick={() => toggleVcenter(vIdx)} sx={{ background:'#d3d3d3',borderRadius:'10px',mb:1 ,borderTop: 1, borderColor: 'divider', boxShadow:2}}  >
-            <VcenterIcon sx={{ mr: 1 }}  />
-            <ListItemText primary={<Link href={vcenter.url} target="_blank" rel="noopener">{vcenter.name}</Link>} />
+          <ListItem
+            button
+            onClick={() => toggleVcenter(vIdx)}
+            sx={{
+              background: "#EDF3F3",
+              borderRadius: "10px",
+              mb: 1,
+              borderTop: 1,
+              borderColor: "divider",
+              boxShadow: 2,
+            }}
+          >
+            <VcenterIcon sx={{ mr: 1 }} />
+            <ListItemText
+              primary={
+                <Link href={vcenter.url} target="_blank" rel="noopener">
+                  {vcenter.name}
+                </Link>
+              }
+            />
             {openStates[vIdx].vcenter ? <ExpandLess /> : <ExpandMore />}
           </ListItem>
           <Collapse in={openStates[vIdx].vcenter} timeout="auto" unmountOnExit>
             <List component="div" disablePadding>
               {vcenter.datacenters.map((datacenter, dIdx) => (
                 <React.Fragment key={dIdx}>
-                  <ListItem button onClick={() => toggleDatacenter(vIdx, dIdx)} sx={{ pl: 4 }}>
+                  <ListItem
+                    button
+                    onClick={() => toggleDatacenter(vIdx, dIdx)}
+                    sx={{ pl: 4 }}
+                  >
                     <DataCenterIcon sx={{ mr: 1 }} />
                     <ListItemText inset primary={datacenter.name} />
-                    {openStates[vIdx].datacenters[dIdx].datacenter ? <ExpandLess /> : <ExpandMore />}
+                    {openStates[vIdx].datacenters[dIdx].datacenter ? (
+                      <ExpandLess />
+                    ) : (
+                      <ExpandMore />
+                    )}
                   </ListItem>
-                  <Collapse in={openStates[vIdx].datacenters[dIdx].datacenter} timeout="auto" unmountOnExit>
+                  <Collapse
+                    in={openStates[vIdx].datacenters[dIdx].datacenter}
+                    timeout="auto"
+                    unmountOnExit
+                  >
                     <List component="div" disablePadding>
                       {datacenter.clusters.map((cluster, cIdx) => (
                         <React.Fragment key={cIdx}>
-                          <ListItem button onClick={() => toggleCluster(vIdx, dIdx, cIdx)} sx={{ pl: 6 }}>
+                          <ListItem
+                            button
+                            onClick={() => toggleCluster(vIdx, dIdx, cIdx)}
+                            sx={{ pl: 6 }}
+                          >
                             <ClusterIcon sx={{ mr: 1 }} />
                             <ListItemText inset primary={cluster.name} />
-                            {openStates[vIdx].datacenters[dIdx].clusters[cIdx].cluster ? <ExpandLess /> : <ExpandMore />}
+                            {openStates[vIdx].datacenters[dIdx].clusters[cIdx]
+                              .cluster ? (
+                              <ExpandLess />
+                            ) : (
+                              <ExpandMore />
+                            )}
                           </ListItem>
-                          <Collapse in={openStates[vIdx].datacenters[dIdx].clusters[cIdx].cluster} timeout="auto" unmountOnExit>
+                          <Collapse
+                            in={
+                              openStates[vIdx].datacenters[dIdx].clusters[cIdx]
+                                .cluster
+                            }
+                            timeout="auto"
+                            unmountOnExit
+                          >
                             <List component="div" disablePadding>
                               {cluster.hosts.map((host, hIdx) => (
                                 <React.Fragment key={hIdx}>
-                                  <ListItem button onClick={() => toggleHost(vIdx, dIdx, cIdx, hIdx)} sx={{ pl: 8 }}>
+                                  <ListItem
+                                    button
+                                    onClick={() =>
+                                      toggleHost(vIdx, dIdx, cIdx, hIdx)
+                                    }
+                                    sx={{ pl: 8 }}
+                                  >
                                     <HostIcon sx={{ mr: 1 }} />
-                                    <ListItemText 
-                                      inset 
-                                      primary={<Link href={host.url} target="_blank" rel="noopener">{host.name}</Link>} />
-                                    {openStates[vIdx].datacenters[dIdx].clusters[cIdx].hosts[hIdx] ? <ExpandLess /> : <ExpandMore />}
+                                    <ListItemText
+                                      inset
+                                      primary={
+                                        <Link
+                                          href={host.url}
+                                          target="_blank"
+                                          rel="noopener"
+                                        >
+                                          {host.name}
+                                        </Link>
+                                      }
+                                    />
+                                    {openStates[vIdx].datacenters[dIdx]
+                                      .clusters[cIdx].hosts[hIdx] ? (
+                                      <ExpandLess />
+                                    ) : (
+                                      <ExpandMore />
+                                    )}
                                   </ListItem>
-                                  <Collapse in={openStates[vIdx].datacenters[dIdx].clusters[cIdx].hosts[hIdx]} timeout="auto" unmountOnExit>
+                                  <Collapse
+                                    in={
+                                      openStates[vIdx].datacenters[dIdx]
+                                        .clusters[cIdx].hosts[hIdx]
+                                    }
+                                    timeout="auto"
+                                    unmountOnExit
+                                  >
                                     <ListItem sx={{ pl: 10 }}>
                                       <IloIcon sx={{ mr: 1 }} />
-                                      <ListItemText 
-                                        inset 
-                                        secondary={<Link href={host.iloUrl} target="_blank" rel="noopener"> {host.iloUrl}</Link>} />
+                                      <ListItemText
+                                        inset
+                                        secondary={
+                                          <Link
+                                            href={host.iloUrl}
+                                            target="_blank"
+                                            rel="noopener"
+                                          >
+                                            {" "}
+                                            {host.iloUrl}
+                                          </Link>
+                                        }
+                                      />
                                     </ListItem>
                                   </Collapse>
                                 </React.Fragment>
